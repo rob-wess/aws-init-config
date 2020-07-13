20c28550418abe80a912b716b21d094d920d414f
Robert Wessinger <53195588+rob-wess@users.noreply.github.com>
2020-07-13 14:09:13 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# Exit if error
+set -e
+
 # New user & password
 HOST_NAME=''
 NEW_USER=''
 NEW_PASS=''
+
 useradd $NEW_USER
 usermod -a -G wheel $NEW_USER
 echo "$NEW_PASS" | passwd --stdin $NEW_USER
@@ -19,10 +23,17 @@ hostnamectl set-hostname $HOST_NAME
 yum update -y
 
 # Install custom packages
-yum install unzip wget vim psmisc setroubleshoot-server bash-completion rsync curl tuned bind-utils firewalld rsync epel-release -y
+
+source /etc/os-release
+
+if [[ "$NAME" == "Fedora" ]]; then
+        dnf install unzip wget vim psmisc setroubleshoot-server bash-completion rsync curl tuned bind-utils firewalld rsync -y
+elif [[ "$NAME" == "CentOS" ]]; then
+        yum install unzip wget vim psmisc setroubleshoot-server bash-completion rsync curl tuned bind-utils firewalld rsync epel-release -y
+fi
 
 # Configure Firewalld
 systemctl enable --now firewalld
 firewall-cmd --set-default-zone=public
-firewall-cmd --zone=public --permanent --add-port={22,80,443}/tcp 
+firewall-cmd --zone=public --permanent --add-port={22,80,443}/tcp
 firewall-cmd --reload
